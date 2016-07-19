c8a400ba814a60e6b3f319d4e56f0a84c0878c34
Tommaso Cucinotta <tommaso@lyx.org>
2016-07-19 11:51:38 +0200

    Adding missing .h files.

---
+++ mycpudeadline_split.h
@@ -0,0 +1,34 @@
+#ifndef _LINUX_CPUDL_H
+#define _LINUX_CPUDL_H
+
+#include <linux/sched.h>
+#include <linux/sched/deadline.h>
+
+#define IDX_INVALID     -1
+
+struct mycpudl_item {
+	u64 dl;
+	int cpu;
+	int idx;
+};
+
+struct mycpudl {
+	raw_spinlock_t lock;
+	int size;
+	cpumask_var_t free_cpus;
+	struct mycpudl_item *elements;
+};
+
+
+#ifdef CONFIG_SMP
+int mycpudl_find(struct mycpudl *cp, struct task_struct *p,
+	       struct cpumask *later_mask);
+void mycpudl_set(struct mycpudl *cp, int cpu, u64 dl);
+void mycpudl_clear(struct mycpudl *cp, int cpu);
+int mycpudl_init(struct mycpudl *cp, int nr_cpu_ids);
+void mycpudl_set_freecpu(struct mycpudl *cp, int cpu);
+void mycpudl_clear_freecpu(struct mycpudl *cp, int cpu);
+void mycpudl_cleanup(struct mycpudl *cp);
+#endif /* CONFIG_SMP */
+
+#endif /* _LINUX_CPUDL_H */
